6b1304915db7d3b204fc1cabcf4b56e5e7405d95
danineuss <daniel.neuss@hey.com>
2024-05-16 11:48:01 +0200

    Checkpoint: sphere reacts to presence of a qr-code.

--- src/components/qr-tracker.js
+++ src/components/qr-tracker.js
@@ -1,4 +1,5 @@
 import foo from '../logic';
+import jsQR from 'jsqr';
 
 AFRAME.registerComponent('qr-tracker', {
   init: function() {
@@ -23,10 +24,16 @@ AFRAME.registerComponent('qr-tracker', {
   tick: function() {
     var imageData = this.canvas.getImageData(0, 0, this.canvasElement.width, this.canvasElement.height);
     var a = 1;
-    // var code = jsQR(imageData.data, imageData.width, imageData.height);
+    var code = jsQR(imageData.data, imageData.width, imageData.height);
 
-    // if (code) {
-    //   console.log(code.data);
-    // }
+    if (code) {
+      console.log("code")
+      
+      var xPos = (code.location.topLeftCorner.x - imageData.width / 2) / imageData.width * 20;
+      var yPos = (imageData.height * 0.8 - code.location.topLeftCorner.y) / imageData.height * 5;
+      this.el.setAttribute("position", {x: xPos, y: yPos, z: -5});
+    }
+    
+    requestAnimationFrame(this.tick);
   }
 });
