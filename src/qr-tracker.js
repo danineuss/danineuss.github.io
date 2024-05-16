22f2e4f8f425d3d28ec2076738452dfaab94b09c
danineuss <daniel.neuss@hey.com>
2024-05-16 09:02:58 +0200

    src: add .ts, .js and .css files.

---
+++ src/qr-tracker.js
@@ -0,0 +1,31 @@
+import foo from './logic';
+
+AFRAME.registerComponent('qr-tracker', {
+  init: function() {
+    // this.canvasElement = document.getElementById("overlayCanvas");
+    // this.canvas = this.canvasElement.getContext("2d");
+
+    foo();
+
+    // console.log(jsQR);
+    // canvas.width = window.innerWidth;
+    // canvas.height = window.innerHeight;
+
+    // var centerX = canvas.width / 2;
+    // var centerY = canvas.height / 2;
+    // var radius = 50;
+    // this.context.beginPath();
+    // this.context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    // this.context.fillStyle = 'red';
+    // this.context.fill();
+  },
+
+  tick: function() {
+    // var imageData = this.canvas.getImageData(0, 0, this.canvasElement.width, this.canvasElement.height);
+    // var code = jsQR(imageData.data, imageData.width, imageData.height);
+
+    // if (code) {
+    //   console.log(code.data);
+    // }
+  }
+});
