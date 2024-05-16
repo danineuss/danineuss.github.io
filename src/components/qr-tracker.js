4c18c190f4a1f45b50f09c67232d844589659f2c
danineuss <daniel.neuss@hey.com>
2024-05-16 12:54:49 +0200

    Checkpoint: possibly calling "locate" properly-ish.

--- src/components/qr-tracker.js
+++ src/components/qr-tracker.js
@@ -1,39 +1,36 @@
-import foo from '../logic';
-import jsQR from 'jsqr';
+// import foo from '../logic';
+// import jsQR from 'jsqr';
+// import { locate, BitMatrix } from '../../node_modules/jsqr/dist/locator/index.d.ts';
+// import { BitMatrix } from '../../node_modules/jsqr/dist/BitMatrix.d.ts';
+// import { locate, BitMatrix } from "jsqr";
+// import { locate, BitMatrix } from "jsqr/dist/locator/";
+import { locate } from "../../libs/jsQR/locator"
+import { BitMatrix } from "../../libs/jsQR/BitMatrix";
 
 AFRAME.registerComponent('qr-tracker', {
   init: function() {
     this.canvasElement = document.getElementById("overlayCanvas");
     this.canvas = this.canvasElement.getContext("2d");
-
-    foo();
-
-    // console.log(jsQR);
-    // canvas.width = window.innerWidth;
-    // canvas.height = window.innerHeight;
-
-    // var centerX = canvas.width / 2;
-    // var centerY = canvas.height / 2;
-    // var radius = 50;
-    // this.context.beginPath();
-    // this.context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-    // this.context.fillStyle = 'red';
-    // this.context.fill();
   },
 
   tick: function() {
     var imageData = this.canvas.getImageData(0, 0, this.canvasElement.width, this.canvasElement.height);
-    var a = 1;
-    var code = jsQR(imageData.data, imageData.width, imageData.height);
-
-    if (code) {
-      console.log("code")
-      
-      var xPos = (code.location.topLeftCorner.x - imageData.width / 2) / imageData.width * 20;
-      var yPos = (imageData.height * 0.8 - code.location.topLeftCorner.y) / imageData.height * 5;
-      this.el.setAttribute("position", {x: xPos, y: yPos, z: -5});
-    }
+    var bitMatrix = new BitMatrix(imageData, imageData.width);
+    var location = locate(bitMatrix);
     
-    requestAnimationFrame(this.tick);
+    if (location){
+      console.log("found" + location[0].topLeft.x);
+    }
+    // var code = jsQR(imageData.data, imageData.width, imageData.height);
+
+    // if (code) {
+    //   console.log("code")
+      
+    //   var xPos = (code.location.topLeftCorner.x - imageData.width / 2) / imageData.width * 20;
+    //   var yPos = (imageData.height * 0.8 - code.location.topLeftCorner.y) / imageData.height * 5;
+    //   this.el.setAttribute("position", {x: xPos, y: yPos, z: -5});
+    // }
+    
+    // requestAnimationFrame(this.tick);
   }
 });
